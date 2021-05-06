9470ec313b8c08b520921d8bf0f60db5f6fb09f2
bilkn <bilkan.konus@hotmail.com>
2021-05-06 22:04:34 +0300

    feat: add content component

---
+++ src/components/content/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Content({ children, ...rest }) {
+  return (
+    <div className="content" {...rest}>
+      {children}
+    </div>
+  );
+}
+
+Content.Wrapper = function ContentWrapper({ children, ...rest }) {
+  return (
+    <div className="content__wrapper" {...rest}>
+      {children}
+    </div>
+  );
+};
+
+Content.Heading = function ContentHeading({ children, ...rest }) {
+  return (
+    <h2 className="content__heading" {...rest}>
+      {children}
+    </h2>
+  );
+};
+
+Content.Text = function ContentText({ children, ...rest }) {
+  return (
+    <p className="content__text" {...rest}>
+      {children}
+    </p>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from './hero';
 export { default as Img } from './img';
+export { default as Content } from './content';
